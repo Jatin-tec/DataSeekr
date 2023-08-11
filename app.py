b68a546dc6057da02e9a5e099c07a7452b98462e
jatin-tec <jatin.kshatriya2821@gmail.com>
2023-08-12 02:55:51 +0530

    updated promt

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ from langchain.llms import OpenAI
 from langchain.chains import ChatVectorDBChain, ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
 from langchain import HuggingFaceHub
-
+from langchain.prompts.chat import (
+    ChatPromptTemplate, 
+    SystemMessagePromptTemplate, 
+    HumanMessagePromptTemplate
+)
 import weaviate
 from utils.htmlTemplate import css, bot_template, user_template
 import os
@@ -17,24 +21,46 @@ def get_conversation_chain(vectorstore):
     momory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, verbose=True)
     # llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.7, "max_length": 10000})
     llm = OpenAI(temperature=0.8, max_tokens=1000)
+
+    system_template = """Use following pieces of context to answer the users question. 
+    Following contexts are summarised texts from users inbox.
+    ----------------
+    {context}"""
+
+    # Create the chat prompt templates
+    messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}")
+    ]
+    qa_prompt = ChatPromptTemplate.from_messages(messages)
+    
+    retriver = vectorstore.as_retriever(search_kwargs={"k": 10})
+
     conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(search_kwargs={"k": 10}), 
-        max_tokens_limit=3000,
-        memory=momory,
-        verbose=True
+    llm=llm,
+    retriever=retriver, 
+    return_source_documents=True,
+    combine_docs_chain_kwargs={"prompt": qa_prompt},
+    max_tokens_limit=3000,
+    memory=momory,
+    verbose=True
     )
+    print(f"Conversation chain created {conversation_chain}")
     return conversation_chain
 
 def user_input(user_query):
+    print(f"User query: {user_query}")
     response =  st.session_state.conversation({"question": user_query})
+    print(f"Response: {response}")
+
     st.session_state.chat_history = response['chat_history']
+    answer = response['answer']
     
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
             st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
         else:
-            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(bot_template.replace("{{MSG}}", answer), unsafe_allow_html=True)
 
 def main():
     load_dotenv()
@@ -58,7 +84,6 @@ def main():
 
     if st.button("Search"):
         with st.spinner("Searching..."):
-            st.session_state.conversation = get_conversation_chain(vectorstore)
             if question:
                 user_input(question)
 
